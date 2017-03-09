7392faa546a44b9302429f1971d55d8807345f62
William Yang <mscame@gmail.com>
2017-03-09 22:44:59 +0100

    commoon

--- src/xiaowang_go/runner/runner_test.go
+++ src/xiaowang_go/runner/runner_test.go
@@ -13,18 +13,23 @@ func TestRun(t *testing.T) {
 	from := "../testdata/test.csv"
 	to := "../testdata/result.csv"
 	runner.Run("../testdata/testcfg.yml", from, to)
+	compare_csv_file(from, to, t)
+}
 
+func compare_csv_file(file1, file2 string, t *testing.T) {
 	//now check file identical
-	src, err := os.Open(from)
+	src, err := os.Open(file1)
 	if err != nil {
 		t.Error(err)
 	}
-	dest, err := os.Open(to)
+	dest, err := os.Open(file2)
 	if err != nil {
 		t.Error(err)
 	}
+
 	s := csv.NewReader(src)
 	d := csv.NewReader(dest)
+
 	for {
 		data1, err1 := s.Read()
 		data2, err2 := d.Read()
@@ -33,7 +38,7 @@ func TestRun(t *testing.T) {
 		} else if err1 != nil || err2 != nil {
 			t.Error("compare data failed, one of the read failed", err1, err2)
 		} else if !reflect.DeepEqual(data1, data2) {
-			t.Errorf("line by line compared failed \n%s\n%s\n", data1, data2)
+			t.Errorf("line by line comparing failed \n%s\n%s\n", data1, data2)
 		}
 	}
 }
